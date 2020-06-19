3dadbb9bb0f68430dd77fb42b9fa5a1bc546ac74
whylikethat <ad591085@ens.uqam.ca>
2020-06-18 23:23:33 -0400

    added app password

--- notification.py
+++ notification.py
@@ -5,7 +5,7 @@ f = open("new.txt", "rb")
 message += f.read()
 
 server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-server.login("psylock2015@gmail.com", "petasse2015")
+server.login("psylock2015@gmail.com", "ldippnffmhsyavjj")
 server.sendmail("psylock2015@gmail.com", "doemae19@gmail.com", message) 
 server.quit()
 
